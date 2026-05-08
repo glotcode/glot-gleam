aa001391e233450bbca3c8f4fe7d8deeb6bd92c0
Petter Rasmussen <petter@hask.no>
2026-05-08 21:38:50 +0200

    Seed database

--- reset_db.sh
+++ reset_db.sh
@@ -5,6 +5,12 @@
     cd glot_backend
     docker compose down
     docker compose up -d
-)
 
+    until docker exec glot_backend-postgres-1 pg_isready -U glot -d glot >/dev/null 2>&1
+    do
+        sleep 1
+    done
+
+    docker exec -i glot_backend-postgres-1 psql -U glot -d glot < ../seed.sql
+)
 
